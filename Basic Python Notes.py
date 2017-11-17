2b21afd09fea8c63d5fb2ea811b000ca9b0f57b8
RosieRivera57355 <33767543+RosieRivera57355@users.noreply.github.com>
2017-11-17 15:26:00 -0800

    Initial commit

---
+++ Basic Python Notes.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print("Welcome to Class!")
+'''This is a bunch of text
+ that will all be ignored
+ because it is in between 3 quotation
+  marks'''
+
+print("Welcome to Class!")
+
+# Variables
+a = "Hello World"
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1-(-5))
